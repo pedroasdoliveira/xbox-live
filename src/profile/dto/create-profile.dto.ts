c2d0a6fd1db13ee5f77e8ad0687b07d845a16f9b
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-05-25 15:48:27 -0300

    feat(profile): implement Create Dto

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProfileDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl, IsUUID } from "class-validator";
+
+export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do perfil',
+    example: 'Player_1'
+  })
+  title: string
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil',
+    example: 'https://i.pinimg.com/280x280_RS/ca/94/d0/ca94d0af95d038459ed4d6bfde48f6e7.jpg'
+  })
+  imageUrl: string
+
+  @IsUUID()
+  @ApiProperty({
+    description: 'Id do usuário',
+    example: ''
+  })
+  userId: string;
+
+  @IsUUID()
+  @ApiProperty({
+    description: 'Gênero do jogo',
+    example: ''
+  })
+  genderName: string;
+
+  @IsUUID(undefined, {each: true})
+  @ApiProperty({
+    description: 'Jogos',
+    example: '',
+  })
+  game: string[]
+}
