76cd740e40e597883e2ad6ca1a83159848ec2ea5
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-05-30 20:56:06 -0300

    feat(gender): implement 'gamesId' as optional

--- src/genders/dto/create-gender.dto.ts
+++ src/genders/dto/create-gender.dto.ts
@@ -16,7 +16,7 @@ export class CreateGenderDto {
     description: 'Id do jogo a ser adicionado no gênero',
     example: '6e6c4e46-37dd-4bfc-8abc-495e8b151b40'
   })
-  gamesId: string;
+  gamesId?: string;
 
   // @ValidateNested({
   //   each: true
