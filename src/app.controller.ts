98017979ec1cde8f36d85c2feb83e3907dd0a861
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-06-06 14:54:00 -0300

    chore: implement http methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @ApiTags('status')
@@ -11,7 +12,8 @@ export class AppController {
   @ApiOperation({
     summary: "Visualizar status da aplicação",
   })
-  getAppStatus(): string {
-    return this.appService.getAppStatus();
+  getAppStatus(@Req() req: Request) {
+    const baseUrl = req.protocol + '://' + req.get('host')
+    return this.appService.getAppStatus(baseUrl);
   }
 }
