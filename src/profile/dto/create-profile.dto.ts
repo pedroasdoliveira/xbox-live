35af56c1e3f07abe326cf89fcddc4151e8bd22d0
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-20 22:34:09 -0300

    feat(create-profile-dto): remove gamesId example

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -25,12 +25,6 @@ export class CreateProfileDto {
   })
   userId: string;
 
-  @ApiProperty({
-    description: 'Id do jogo a ser adicionado no perfil',
-    example: 'fbe611b7-34b1-4739-8b9e-418765b69942',
-  })
-  gamesId?: string;
-
   // @ValidateNested({
   //   each: true
   // })
